a0ddeef40f26df3086984789cf7149c824ac07dc
asnashameel <155845311+asnashameel@users.noreply.github.com>
2025-06-15 20:40:18 +0530

    Update backend.tf
    changed

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "bootcamp1-tf-state-dev"
+    bucket         = "my-tf-state-bucket-7736"
     key            = "global/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "bootcamp1-terraform-lock-table-dev"
+    dynamodb_table = "terraform-lock"
     encrypt        = true
   }
 }
